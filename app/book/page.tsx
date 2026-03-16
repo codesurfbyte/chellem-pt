cbe6ec8fd8a81ee6be7f9eade651e2a051f7604c
codesurfbyte <codesurfbyte@gmail.com>
2026-03-16 14:44:37 +0900

    Restore SSR for booking page

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -1,22 +1,4 @@
-import dynamic from 'next/dynamic'
-
-const WeeklyCalendar = dynamic(() => import('@/components/WeeklyCalendar'), {
-  ssr: false,
-  loading: () => (
-    <div className="space-y-3">
-      {[...Array(3)].map((_, i) => (
-        <div key={i} className="card p-4 animate-pulse">
-          <div className="h-4 bg-sand rounded w-20 mb-3" />
-          <div className="grid grid-cols-2 md:grid-cols-3 gap-2">
-            {[...Array(3)].map((_, j) => (
-              <div key={j} className="h-16 bg-sand rounded-lg" />
-            ))}
-          </div>
-        </div>
-      ))}
-    </div>
-  ),
-})
+import WeeklyCalendar from '@/components/WeeklyCalendar'
 
 export default function BookPage() {
   return (
